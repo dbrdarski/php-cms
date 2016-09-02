85ebc6ff2c325769329a3d0109b321a152c907f3
Dane Brdarski <dane.brdarski@gmail.com>
2016-09-02 07:47:49 +0200

    Created validator class

---
+++ core/Validation/Validator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Core\Validation;
+
+class Validator{
+
+    function __construct($val)
+    {
+        $this->value = $val;
+    }
+    private $value;
+    private statitc $faliures = array();
+    private statitc $validators = array(
+        'numeric'       => "/^[0-9]*$/",
+        'alpha'         => "/^[a-zA-Z0-9_]*$/",
+        'alphanumeric'  => "/^[a-zA-Z0-9_]*$/",
+        'alphanumeric_' => "/^[a-zA-Z0-9]*$/",
+        'email'         => "/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD"       
+    );    
+
+    public function __call($method, $arguments)
+    {       
+        $pattern = $validators[$method];
+        $message = array_shift($arguments);
+        $test = preg_match($pattern, $this->value);
+        if(!$test){
+            $faliures[$pattern] = $message ? $message : $pattern;
+        }
+        return $this;
+    }
+    public function pattern($pattern, $name, $message = null){
+        $test = preg_match($pattern, $this->value);
+        if(!$test){
+            $faliures[$name] = $message ? $message : $name;
+        }
+        return $this;
+    }
+}
